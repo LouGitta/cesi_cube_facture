2d7adf02c1e0eb04ccd46f6de517cd5aa005c5b5
AboubacarDby <diabyaboubacar940@outlook.fr>
2025-01-16 11:25:06 +0100

    add entity Facture

---
+++ crm-factures/src/Entity/Facture.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FactureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FactureRepository::class)]
+class Facture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $exercice_id = null;
+
+    #[ORM\Column]
+    private ?int $client_id = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $num = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $nb_jour = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $crea = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $info = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $sent = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $payed = null;
+
+    #[ORM\Column]
+    private ?float $tva = null;
+
+    #[ORM\Column]
+    private ?float $ttc = null;
+
+    #[ORM\Column]
+    private ?float $ht = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getExerciceId(): ?int
+    {
+        return $this->exercice_id;
+    }
+
+    public function setExerciceId(int $exercice_id): static
+    {
+        $this->exercice_id = $exercice_id;
+
+        return $this;
+    }
+
+    public function getClientId(): ?int
+    {
+        return $this->client_id;
+    }
+
+    public function setClientId(int $client_id): static
+    {
+        $this->client_id = $client_id;
+
+        return $this;
+    }
+
+    public function getNum(): ?string
+    {
+        return $this->num;
+    }
+
+    public function setNum(string $num): static
+    {
+        $this->num = $num;
+
+        return $this;
+    }
+
+    public function getNbJour(): ?int
+    {
+        return $this->nb_jour;
+    }
+
+    public function setNbJour(int $nb_jour): static
+    {
+        $this->nb_jour = $nb_jour;
+
+        return $this;
+    }
+
+    public function getCrea(): ?\DateTimeInterface
+    {
+        return $this->crea;
+    }
+
+    public function setCrea(\DateTimeInterface $crea): static
+    {
+        $this->crea = $crea;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getInfo(): ?string
+    {
+        return $this->info;
+    }
+
+    public function setInfo(string $info): static
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+
+    public function getSent(): ?\DateTimeInterface
+    {
+        return $this->sent;
+    }
+
+    public function setSent(\DateTimeInterface $sent): static
+    {
+        $this->sent = $sent;
+
+        return $this;
+    }
+
+    public function getPayed(): ?\DateTimeInterface
+    {
+        return $this->payed;
+    }
+
+    public function setPayed(\DateTimeInterface $payed): static
+    {
+        $this->payed = $payed;
+
+        return $this;
+    }
+
+    public function getTva(): ?float
+    {
+        return $this->tva;
+    }
+
+    public function setTva(float $tva): static
+    {
+        $this->tva = $tva;
+
+        return $this;
+    }
+
+    public function getTtc(): ?float
+    {
+        return $this->ttc;
+    }
+
+    public function setTtc(float $ttc): static
+    {
+        $this->ttc = $ttc;
+
+        return $this;
+    }
+
+    public function getHt(): ?float
+    {
+        return $this->ht;
+    }
+
+    public function setHt(float $ht): static
+    {
+        $this->ht = $ht;
+
+        return $this;
+    }
+}
